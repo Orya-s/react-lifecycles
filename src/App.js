ec436e0b8a1e753d09195ddf01668682d39d96d6
orya <orya.sp@gmail.com>
2022-11-13 18:29:18 +0200

    Add my solution

--- src/App.js
+++ src/App.js
@@ -3,13 +3,71 @@ import axios from 'axios'
 
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      users: []
+    }
+  }
+
+  async getUsers() {
+    return axios.get("https://jsonplaceholder.typicode.com/users")
+  }
+
+  async componentDidMount() {
+    const response = await this.getUsers()
+    this.setState({ users: response.data })
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello, World!</h1>
+      <div className="App" id="people">
+        {this.state.users.map(u => {
+          return <User key={u.id} userData={u} />
+        })}
       </div>
     );
   }
 }
 
-export default App;
+
+
+
+
+class User extends Component {
+  render() {
+    const user = this.props.userData
+    return (
+      <div className="person"><div className="name">{user.name}</div><div>{user.email}</div></div>
+    )
+  }
+}
+
+
+
+
+
+class Timer extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      time: new Date(),
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ time: new Date() })
+      console.log("Updated time")
+    }, 1000)
+  }
+
+  render() {
+    return <div> {this.state.time.toLocaleTimeString()}</div >
+  }
+}
